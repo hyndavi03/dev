f1c212c37649c845862a0a58648ad4aadc475b3c
ratnakottara <ratnakottara@gmail.com>
2024-05-31 02:09:55 +0530

    some tf file

--- terraform/service.tf
+++ terraform/service.tf
@@ -1,6 +1,15 @@
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecs-task-execution-role"
-  # other role configurations
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action    = "sts:AssumeRole"
+    }]
+  })
 }
 
 resource "aws_ecs_task_definition" "notification_task" {
