f917f0775bd93be2d1b699d7cf7ba1bcc45574da
ratnakottara <ratnakottara@gmail.com>
2024-05-31 15:20:28 +0530

    retry

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,11 @@
 variable "region" {
   default = "ap-south-1"
 }
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type    = list(string)
+  default = []  # Set default value if needed
+}
