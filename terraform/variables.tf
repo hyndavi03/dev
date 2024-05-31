9607ceee7d4c90dfc40a58be84add8f7e04c9335
ratnakottara <ratnakottara@gmail.com>
2024-06-01 01:10:53 +0530

    hope

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "vpc_id" {
   description = "The ID of the VPC"
   type        = string
+  default = "vpc-0fb809d348139ac47"
 }
 
 variable "subnet_ids" {
   description = "List of public subnet IDs"
   type        = string
+  default = "subnet-03b108e1dafc6f9e5"
 }
