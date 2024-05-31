3ae8c555483e0718819600c2fdef18c26bfbbc97
ratnakottara <ratnakottara@gmail.com>
2024-05-31 12:03:58 +0530

    updated

---
+++ terraform/alb.tf
@@ -0,0 +1,62 @@
+resource "aws_lb" "main" {
+  name               = "notification-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = var.subnet_ids
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = "notification-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-299"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
+resource "aws_security_group" "lb_sg" {
+  name        = "lb_sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "load_balancer_dns_name" {
+  value = aws_lb.main.dns_name
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,11 @@
 variable "region" {
   default = "ap-south-1"
 }
+variable "vpc_id" {
+  description = "The VPC ID where the ECS cluster is deployed"
+}
+
+variable "subnet_ids" {
+  description = "The subnets IDs where the ECS cluster is deployed"
+  type        = list(string)
+}
