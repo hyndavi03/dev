51c8c22af9091178a46b82910b8d5b47e9bd165c
ratnakottara <ratnakottara@gmail.com>
2024-05-31 16:59:38 +0530

    modify

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,12 @@ variable "region" {
   default     = "ap-south-1"
 }
 
+variable "subnet_ids" {
+  description = "A list of subnet IDs to associate with the load balancer"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
