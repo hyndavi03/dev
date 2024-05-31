a70cfebb201975f361dde59f83917065e87ea498
ratnakottara <ratnakottara@gmail.com>
2024-05-31 23:32:19 +0530

    modified

--- terraform/service.tf
+++ terraform/service.tf
@@ -1,12 +1,69 @@
+# terraform/ecs.tf
+
+resource "aws_ecs_cluster" "notification_cluster" {
+  name = "notification_cluster"
+}
+
+resource "aws_ecs_task_definition" "notification_task" {
+  family                   = "notification-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions    = jsonencode([
+    {
+      name      = "notification-container"
+      image     = "${aws_account_id}.dkr.ecr.ap-south-1.amazonaws.com/notification-service:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
 resource "aws_ecs_service" "notification_service" {
-  name            = "notification_service"
-  cluster         = aws_ecs_cluster.mycluster.id
+  name            = "notification-service"
+  cluster         = aws_ecs_cluster.notification_cluster.id
   task_definition = aws_ecs_task_definition.notification_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
   network_configuration {
-    subnets         = ["subnet-03b108e1dafc6f9e5", "subnet-0f47b25e174d49d09"]
+    subnets          = ["subnet-XXXXXXXX"]
+    security_groups  = ["sg-XXXXXXXX"]
     assign_public_ip = true
   }
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Effect = "Allow"
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+  ]
+}
+
+resource "aws_ecr_repository" "notification_service" {
+  name                 = "notification-service"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
