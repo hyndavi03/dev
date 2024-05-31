19f4e2dfd0a38855a23c0ea711c83f6c9675e74a
ratnakottara <ratnakottara@gmail.com>
2024-05-31 16:29:55 +0530

    modify

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,51 +1,59 @@
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecs_task_execution_role"
+  name               = "ecs_task_execution_role"
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
+      Effect = "Allow"
       Principal = {
         Service = "ecs-tasks.amazonaws.com"
       }
+      Action = "sts:AssumeRole"
     }]
   })
 }
 
-resource "aws_ecs_task_definition" "notification_task" {
-  family                   = "notification-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  container_definitions    = jsonencode([{
-    name  = "notification-service"
-    image = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/notification-service:latest"
-    essential = true
-    portMappings = [{
-      containerPort = 3000
-      hostPort      = 3000
-    }]
-  }])
+resource "aws_lb" "main" {
+  name               = "notification-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = var.subnet_ids
+
+  enable_deletion_protection = false
 }
 
-resource "aws_ecs_service" "notification_service" {
-  name            = "notification_service"
-  cluster         = aws_ecs_cluster.mycluster.id
-  task_definition = aws_ecs_task_definition.notification_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-  network_configuration {
-    subnets         = ["subnet-03b108e1dafc6f9e5", "subnet-0f47b25e174d49d09"]
-    assign_public_ip = true
+resource "aws_lb_target_group" "main" {
+  name     = "notification-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-299"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
   }
 }
 
 resource "aws_security_group" "lb_sg" {
-  name        = "lb_sg-${random_id.lb_sg_suffix.hex}"
+  name        = "lb_sg"
   description = "Allow HTTP inbound traffic"
-  vpc_id      = "vpc-0fb809d348139ac47"
+  vpc_id      = var.vpc_id
 
   ingress {
     description = "HTTP from VPC"
@@ -62,7 +70,6 @@ resource "aws_security_group" "lb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "random_id" "lb_sg_suffix" {
-  byte_length = 4
+output "load_balancer_dns_name" {
+  value = aws_lb.main.dns_name
 }
