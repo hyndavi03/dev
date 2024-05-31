1ce93b990c023da6c78bba3d4b0ab6782e30f74c
ratnakottara <ratnakottara@gmail.com>
2024-06-01 01:02:21 +0530

    waited

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,34 +8,6 @@ resource "aws_lb" "main" {
   enable_deletion_protection = false
 }
 
-resource "aws_lb_target_group" "main" {
-  name     = "notification-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = "vpc-0fb809d348139ac47"
-  target_type = "ip"
-
-  health_check {
-    path                = "/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    matcher             = "200-299"
-  }
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = "80"
-  protocol          = "HTTP"
-  
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
-  }
-}
-
 resource "aws_security_group" "lb_sg" {
   name        = "lb_sg"
   description = "Allow HTTP inbound traffic"
