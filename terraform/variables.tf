0466309e544e26112b702ae6b891f35f12b70f40
ratnakottara <ratnakottara@gmail.com>
2024-05-31 16:50:02 +0530

    modify

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,12 +4,3 @@ variable "region" {
   default     = "ap-south-1"
 }
 
-variable "vpc_id" {
-  description = "The VPC ID where resources will be deployed"
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "A list of subnet IDs where resources will be deployed"
-  type        = list(string)
-}
